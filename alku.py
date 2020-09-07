b4d525ca3f1363e42dd8edf4c2b4a6524aca703f
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-09-07 13:44:20 +0300

    Haetaan activities ja eritellään tags
    tagit on dict kansiossa, next step tehdään lista tägien nimillä joista voi valita, reversataan dicti että voidaan hakea valuella eikä avaimilla ja sitte... emt.

--- alku.py
+++ alku.py
@@ -1,1 +1,15 @@
-committaa stna
+import urllib.request
+import json
+
+JSON_URL = 'http://open-api.myhelsinki.fi/v1/activities/'
+
+def get_activities():
+    with urllib.request.urlopen(JSON_URL) as response:
+        return json.loads(response.read())
+
+def main():
+    activities = get_activities()
+    a_tags = activities['tags']
+
+if __name__=='__main__':
+    main()
