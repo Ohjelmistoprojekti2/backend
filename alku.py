4937d5b94aa8f7bfda689e4fa896d7536d7695e4
joni miettinen <joni9909@hotmail.fi>
2020-09-15 11:13:31 +0300

    activity listan läpi menevä funktio, ei vielä pysty erottamaan tagit

--- alku.py
+++ alku.py
@@ -3,21 +3,40 @@ import json
 
 JSON_URL = 'http://open-api.myhelsinki.fi/v1/activities/'
 
-usertags = raw_input("Kirjoita mielenkiinnonkohteesi: ").strip().upper() #tähän voisi listata pari esimerkki tägiä jsonista jotta käyttäjä pystyisi kirjoittamaan niitä.
+usertags = input("Kirjoita mielenkiinnonkohteesi: ").strip().upper() #tähän voisi listata pari esimerkki tägiä jsonista jotta käyttäjä pystyisi kirjoittamaan niitä.
+
 
 
 def get_activities():
     with urllib.request.urlopen(JSON_URL) as response:
         return json.loads(response.read())
 
+# Fetch the tags from an activity
+def gettags(act):
+    return act['tags']
+
+# Fetch name of an activity.
+def getname(act):
+    return act['name']['fi']
+
 def main():
     activities = get_activities()
     a_tags = activities['tags']
+    activity_list = activities['data']
 
     if usertags in a_tags:
-        print(activities[usertags].title())
+        print(activity_list[usertags].title())
     else:
         print('Tapahtumia ei löytynyt')
+
+    for idx, act in enumerate(activity_list):
+        for x in gettags(act):
+            if usertags == gettags(act):
+                print(getname(act))
+                break
+            print(x['name'])
+        print("\n")
+
 
 if __name__=='__main__':
     main()
